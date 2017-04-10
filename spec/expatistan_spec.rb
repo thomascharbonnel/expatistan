25b5961a0eee34a38336fdb30ede9e3c96f30ab9
Thomas Charbonnel <thomas@charbonnel.eu>
2017-04-10 18:16:44 +0800

    Add compare(Paris, London) test

--- spec/expatistan_spec.rb
+++ spec/expatistan_spec.rb
@@ -1,11 +1,24 @@
 require "spec_helper"
+require "fakeweb"
 
 RSpec.describe Expatistan do
   it "has a version number" do
     expect(Expatistan::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".compare(city_a, city_b)" do
+    context "when city_a is Paris" do
+      context "when city_b is London" do
+        it "returns 1.2" do
+          stream = File.read("paris_london.html")
+          FakeWeb.register_uri(:get, 
+            "https://www.expatistan.com/cost-of-living/comparison/paris/london", 
+            :body => stream, 
+            :content_type => "text/html")
+
+          expect(Expatistan.compare("Paris", "London")).to eq(1.2)
+        end
+      end
+    end
   end
 end
