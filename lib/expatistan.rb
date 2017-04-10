de5168af886f78a91bf5cdc7a17cb8a28a5776be
Thomas Charbonnel <thomas@charbonnel.eu>
2017-04-10 21:57:40 +0800

    Impl .compare and .parse_city_name

--- lib/expatistan.rb
+++ lib/expatistan.rb
@@ -1,5 +1,22 @@
 require "expatistan/version"
+require "mechanize"
 
 module Expatistan
-  # Your code goes here...
+  def self.compare(city_a, city_b)
+    city_a = parse_city_name(city_a)
+    city_b = parse_city_name(city_b)
+
+    a = Mechanize.new
+    page = a.get("https://www.expatistan.com/cost-of-living/comparison/#{city_a}/#{city_b}")
+
+    text = page.search("h1 span.content").text
+
+    if text =~ /more expensive/
+      1 + text.gsub(/% more expensive/, "").to_i / 100.0
+    end
+  end
+
+  def self.parse_city_name(orig)
+    orig.downcase.gsub(/ /, "-")
+  end
 end
